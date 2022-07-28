e71cf51f18650030dfab0b42a068c7dd190c75c0
Alamin Musa Magaga <55305080+alaminmagaga@users.noreply.github.com>
2022-07-27 19:38:10 -0700

    Update settings.py

--- Forum/settings.py
+++ Forum/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -110,10 +111,11 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS=(
+STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static'),
-)
+    ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Managing Media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
